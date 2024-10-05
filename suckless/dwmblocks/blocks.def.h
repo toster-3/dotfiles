9625062a75506115e7443efb702202a4cf560599
toster-3 <git.gains450@passinbox.com>
2024-10-05 22:09:53 +0000

    Add suckless repo

---
+++ suckless/dwmblocks/blocks.def.h
@@ -0,0 +1,14 @@
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
+
+static const Block blocks[] = {
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"", "sh $HOME/.config/dwm/battery.sh", 300, 0},
+    {" ", "date '+%H:%M'", 20, 0},
+};
+
+// sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
+static char delim[] = " ";
+static unsigned int delimLen = 1;
